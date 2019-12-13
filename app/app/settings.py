06672633ff63714eb5a5e5d06a8720fae716e941
Karim Tayie <karimsherif2597@gmail.com>
2019-12-13 23:37:49 +0200

    Added base html and Updated django settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -14,7 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_DIR = BASE_DIR.path('app')
 
 
 # Quick-start development settings - unsuitable for production
